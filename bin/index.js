bf3cbd04bfbff23f5dff22114bd2ad3161bced6f
nahim <nahim@hotmail.be>
2017-12-18 20:45:34 +0100

    init

---
+++ bin/index.js
@@ -0,0 +1,5 @@
+"use strict"
+
+const downloadPhonegapBuilds = require('../lib/download-build-from-phonegap')
+
+return downloadPhonegapBuilds.sendToPhonegapBuild(phonegapAppId, phonegapAuthToken, callback)
