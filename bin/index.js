4b779524dc50fddec946080a8111378fc5fa50e5
nahim <nahim@hotmail.be>
2017-12-18 21:05:02 +0100

    renaming js file

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 "use strict"
 
-const downloadPhonegapBuilds = require('../lib/download-build-from-phonegap')
+const downloadPhonegapBuilds = require('../lib/download-builds-from-phonegap')
 
 return downloadPhonegapBuilds.sendToPhonegapBuild(phonegapAppId, phonegapAuthToken, callback)
